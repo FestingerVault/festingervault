0122e5ae6e999a0bc69c717cc10fb75da25bffe7
Devolution <31021755+devolution007@users.noreply.github.com>
2024-08-27 00:05:40 +0500

    Update data-table.tsx
    rewrite using best practices.

--- src/components/dt/data-table.tsx
+++ src/components/dt/data-table.tsx
@@ -1,99 +1,60 @@
-import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
-} from "@/components/ui/table";
-import type {
-	DataTableFilterableColumn,
-	DataTableSearchableColumn,
-} from "@/types/data-table";
-import {
-	type ColumnDef,
-	type Table as TanstackTable,
-	flexRender,
-} from "@tanstack/react-table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import type { DataTableFilterableColumn, DataTableSearchableColumn } from "@/types/data-table";
+import { type ColumnDef, type Table as TanstackTable, flexRender } from "@tanstack/react-table";
 import { __ } from "@wordpress/i18n";
-import * as React from "react";
+import React from "react";
 import { DataTablePagination } from "./data-table-pagination";
 import { DataTableToolbar } from "./data-table-toolbar";
 
 /**
- * learn more about data-table at shadcn ui website @see https://ui.shadcn.com/docs/components/data-table
- */
+* Access more information on data-table at the ShadCN UI website @see https://ui.shadcn.com/docs/components/data-table
+*/
 
 type DataTableProps<TData, TValue> = {
-    columns: ColumnDef<TData, TValue>[];
-    table: TanstackTable<TData>;
-    //totalRows: number;
-    filterableColumns?: DataTableFilterableColumn<TData>[];
-    searchableColumns?: DataTableSearchableColumn<TData>[];
+    columns: ColumnDef<TData, TValue>[],
+    table: TanstackTable<TData>,
+    filterableColumns?: DataTableFilterableColumn<TData>[],
+    searchableColumns?: DataTableSearchableColumn<TData>[],
 };
 
 export function DataTable<TData, TValue>({
     columns,
     table,
-    //totalRows,
     searchableColumns = [],
     filterableColumns = [],
 }: DataTableProps<TData, TValue>) {
     return (
         <div className="space-y-4">
-            <DataTableToolbar
-                table={table}
-                filterableColumns={filterableColumns}
-                searchableColumns={searchableColumns}
-            />
+            <DataTableToolbar table={table} filterableColumns={filterableColumns} searchableColumns={searchableColumns} />
             <div className="flex-shrink rounded-md border border-border bg-background">
                 <Table>
                     <TableHeader>
                         {table.getHeaderGroups().map((headerGroup) => (
                             <TableRow key={headerGroup.id}>
-                                {headerGroup.headers.map((header) => {
-                                    return (
-                                        <TableHead key={header.id}>
-                                            {header.isPlaceholder
-                                                ? null
-                                                : (flexRender(
-                                                      header.column.columnDef
-                                                          .header,
-                                                      header.getContext(),
-                                                  ) as React.ReactNode)}
-                                        </TableHead>
-                                    );
-                                })}
+                                {headerGroup.headers.map((header) => (
+                                    <TableHead key={header.id}>
+                                        {!header.isPlaceholder ?
+                                             flexRender(header.column.columnDef.header, header.getContext()) 
+                                             : null}
+                                    </TableHead>
+                                ))}
                             </TableRow>
                         ))}
                     </TableHeader>
                     <TableBody>
                         {table.getRowModel().rows?.length ? (
                             table.getRowModel().rows.map((row) => (
-                                <TableRow
-                                    key={row.id}
-                                    data-state={
-                                        row.getIsSelected() && "selected"
-                                    }
-                                >
+                                <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
                                     {row.getVisibleCells().map((cell) => (
                                         <TableCell key={cell.id}>
-                                            {
-                                                flexRender(
-                                                    cell.column.columnDef.cell,
-                                                    cell.getContext(),
-                                                ) as React.ReactNode
-                                            }
+                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
                                         </TableCell>
                                     ))}
                                 </TableRow>
                             ))
                         ) : (
                             <TableRow>
-                                <TableCell
-                                    colSpan={columns.length}
-                                    className="h-24 text-center"
-                                >
+                                <TableCell colSpan={columns.length} className="h-24 text-center">
                                     {__("No results", 'festingervault')}.
                                 </TableCell>
                             </TableRow>
